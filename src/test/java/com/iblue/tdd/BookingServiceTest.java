e9e540c9cc7f3377c4aab41edc96e448a34f8fe5
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-26 14:07:41 -0300

    status ok for third test

--- src/test/java/com/iblue/tdd/BookingServiceTest.java
+++ src/test/java/com/iblue/tdd/BookingServiceTest.java
@@ -4,12 +4,24 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.iblue.tdd.service.BookingService;
 
 @RunWith(SpringRunner.class)
 public class BookingServiceTest {
+	
+	@TestConfiguration
+	static class BookingServiceTestConfiguration {
+		
+		@Bean
+		public BookingService bookingService() {
+			return new BookingService();
+		}
+		
+	}
 	
 	@Autowired
 	BookingService bookingService;
