336bce88abaca649aaf774781536f8905c8aa96a
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-26 12:14:06 -0300

    first test

---
+++ src/test/java/com/iblue/tdd/BookingServiceTest.java
@@ -0,0 +1,23 @@
+package com.iblue.tdd;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class BookingServiceTest {
+	
+	@Autowired
+	BookingService bookingService;
+	
+	@Test
+	public void bookingServiceDaysCalculator() {
+		String name = "Lucas";
+		int days = bookingService.daysCalculatorWithDatabase(name);
+		
+		Assertions.assertEquals(days, 10);
+	}
+	
+}
